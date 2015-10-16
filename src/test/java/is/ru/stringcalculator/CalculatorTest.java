f503d8f8331c9a62e546cec246f24fd047e94e19
Steinar Ágúst Steinarsson <steinar@localhost.localdomain>
2015-10-16 17:55:50 +0000

    second test here

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -8,11 +8,28 @@ public class CalculatorTest {
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
-
+    /* First step */
 	@Test
 	public void testAdd() {
 		assertEquals(0, Calculator.add(""));
 		assertEquals(1, Calculator.add("1"));
 		assertEquals(8, Calculator.add("3,5"));
 	}
+
+	/* Second step */
+	@Test
+	public void testAddTriangle() {
+		String test = "";
+		for(int i = 1; i < 100; i++){
+			if(i != 99){
+				test = test + Integer.toString(i) + ",";
+			}
+			else{
+				test += test + Integer.toString(i);
+			}
+		}
+		assertEquals(4950, Calculator.add(test));
+	}
+
+
 }
